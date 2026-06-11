1a26e896a44f4332ea7a9a97081d3bc80193ae0c
Utk1rs8 <164029924+Utk1rs8@users.noreply.github.com>
2026-06-11 16:20:44 +0530

    Add files via upload

---
+++ Da.java
@@ -0,0 +1,21 @@
+public class Da{
+    public static void main(String s[]) {
+        byte b=125;
+        System.out.println("Byte Value" + b);
+        short points = 3000;
+        System.out.println("points scored" + points);
+        int population = 4350000;
+        System.out.println("Population " + population);
+        long age_in_milliseconds = 788923800000l;
+        System.out.println("age" + age_in_milliseconds);
+        char keyboard_input='a';
+        System.out.println("input "+ keyboard_input);
+        boolean islabcomplete=false;
+        System.out.println("lab complete" + islabcomplete);
+        float house_vaue=4000000.58f;
+        System.out.println("value to house"+ house_vaue);
+        double pi = 3.14159265359;
+        System.out.println("The value of pi is" + pi);
+     
+    }
+}
