245ea4d6b0c42599ed413856fdecb848fae86d8e
Utk1rs8 <164029924+Utk1rs8@users.noreply.github.com>
2026-06-17 19:05:06 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,24 @@
+public class Operators {
+    public static void main(String[] args)
+    {
+        int num1=20;
+        int num2=10;
+        System.out.println("Mathematical operator with numbers: " + num1 +" and " + num2);
+        // Addition
+        System.out.println("Addition: " + (num1 + num2));
+        // Substraction
+        System.out.println("Substraction: " + (num1 - num2));
+        // Multiplication
+        System.out.println("Multiplication: " + (num1 * num2));
+        // Division
+        System.out.println("Division: " + (num1 / num2));
+        // Modulus
+        System.out.println("Modulus: " + (num1 % num2));
+
+        // Double for more precise modulus
+        double num3=20.0;
+        double num4=7.0;
+        System.out.println("Precise Modulus: " + (num3 / num4));      
+    }
+    
+}
---
+++ RelationalOperator.java
@@ -0,0 +1,19 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        // Equal to (==)
+        System.out.println("a==b "+ (a==b));
+        // Not Equal to (!=)
+        System.out.println("a!=b "+ (a!=b));
+        // Greater Than (>)
+        System.out.println("a>b "+ (a>b));
+        // Less Than (<)
+        System.out.println("a<b "+ (a<b));
+        // Greater than Equal to (>=)
+        System.out.println("a>=b "+ (a>=b));
+        // Less Than Equal to (<=)
+        System.out.println("a<=b "+ (a<=b));
+    }
+    
+}
