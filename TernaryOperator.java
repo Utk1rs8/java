98504827e7bb66550e9d4b3129e74557065d6d2c
Utk1rs8 <164029924+Utk1rs8@users.noreply.github.com>
2026-06-18 13:44:34 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args)
+    {
+        String hasArgs = args.length == 0 ? "No Arguments Required" : args[0];
+        System.out.println(hasArgs);
+    }
+    
+}
