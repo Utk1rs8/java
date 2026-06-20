9d4209c803e0e3fef028330f3165589d063346a0
Utk1rs8 <164029924+Utk1rs8@users.noreply.github.com>
2026-06-20 16:26:33 +0530

    Add files via upload

---
+++ ArrayAccess4.java
@@ -0,0 +1,9 @@
+public class ArrayAccess4 {
+    public static void main(String s[]){
+        int num_args=s.length;
+        System.out.println("The Length of the array is "+ num_args);
+        for (int i=0;i<num_args;i++){
+            System.out.println(s[i]);
+        }
+    }
+}
