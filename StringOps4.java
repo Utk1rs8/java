e85137cd18fb8d105763be6319aba5a4696d5fd7
Utk1rs8 <164029924+Utk1rs8@users.noreply.github.com>
2026-06-23 16:52:02 +0530

    Add files via upload

---
+++ StringOps4.java
@@ -0,0 +1,12 @@
+public class StringOps4 {
+    public static void main(String s[]) {
+		String s1 = "Washington";
+        String s2= "DC";
+        s1=s1.concat(s2);
+        System.out.println("s1 "+ s1);
+        s1=s1.substring(0,10);
+        System.out.println("s1 "+s1);
+        System.out.println("s1.substring(7,10) "+s1.substring(7,10));
+        System.out.println("s1.substring(7) "+s1.substring(7));
+    }    
+}
